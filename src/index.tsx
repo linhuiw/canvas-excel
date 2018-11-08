4070c727d68b5629de487e3db1775086469724ce
linhuiw <solesnip@gmail.com>
2018-11-08 16:20:47 +0800

    Feat init library

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,15 @@
  * @class ExampleComponent
  */
 
-import * as React from 'react'
+import * as React from 'react';
+import styles from './styles.css';
 
-import styles from './styles.css'
-
-export type Props = { text: string }
+type Props = { text: string };
 
 export default class ExampleComponent extends React.Component<Props> {
   render() {
-    const {
-      text
-    } = this.props
+    const { text } = this.props;
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    return <div className={styles.test}>Example Component: {text}</div>;
   }
 }
